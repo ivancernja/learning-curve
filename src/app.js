f6fc2f617519786bcd13b367174dc612f272121e
Ivan <ivancernja@gmail.com>
2018-06-13 14:24:08 +0200

    implented post fetching + images

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey = "b1f407bc0d6d4b73a3d84f132c95aa74";
+
+function buildUrl (url) {
+	return NYTBaseUrl + url + ".json?api-key=" + ApiKey
+}
+
+const vm = new Vue({
+ 	el: '#app',
+  	data: {
+    	results: []
+	},
+	mounted() {
+		this.getPosts('home');
+  	},
+  	methods: {
+  		getPosts(section) {
+  			let url = buildUrl(section);
+  			axios.get(url).then((response) => {
+  				this.results = response.data.results;
+  				}).catch( error => {console.log(error);})
+  		}
+  	},
+  	computed: {
+  		processedPosts() {
+  			let posts = this.results;
+
+  			posts.map(post => {
+  				let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+  				post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+  			});
+  			let i, j, chunkedArray = [], chunk = 4;
+  			for (i=0, j=0; i < posts.length; i += chunk, j++) {
+  				chunkedArray[j] = posts.slice(i,i+chunk);
+  			}
+  			return chunkedArray;
+  		}
+  	}
+  });
+
